ceee4197981bd72284103be840e0953842896c8a
Kowalski42 <sealenochka@gmail.com>
2022-07-27 01:23:41 +0300

    Initial commit

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,13 +1,13 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
+        $x(".//button//div[text()='Принять все']").click();
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
